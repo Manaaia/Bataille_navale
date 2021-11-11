0a1fc8821390d09bc2299b9fc4cd6cea1b744ac5
Manon <manon23.villard@hotmail.fr>
2021-11-11 23:21:35 +0100

    Game process ok (11112021)

---
+++ app/src/main/java/fr/afpa/bataille_navale/AI.java
@@ -0,0 +1,21 @@
+package fr.afpa.bataille_navale;
+
+import java.util.ArrayList;
+
+public class AI {
+    private int level;
+
+    public AI(int level) {
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+
+}
--- app/src/main/java/fr/afpa/bataille_navale/GameActivity.java
+++ app/src/main/java/fr/afpa/bataille_navale/GameActivity.java
@@ -2,47 +2,59 @@ package fr.afpa.bataille_navale;
 
 import static java.lang.Math.abs;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 public class GameActivity extends AppCompatActivity {
     private GridLayout rivalLayout;
     private GridLayout myLayout;
     private BoatMgr boatMgr;
-    private Boat playerTorpilleur;
-    private Boat playerPorteAvion;
-    private Boat playerCroiseur;
-    private Boat playerContreTorpilleur1;
-    private Boat playerContreTorpilleur2;
-    private Boat rivalTorpilleur;
-    private Boat rivalPorteAvion;
-    private Boat rivalCroiseur;
-    private Boat rivalContreTorpilleur1;
-    private Boat rivalContreTorpilleur2;
     private ArrayList<Boat> rivalFleet;
+    private ArrayList<Boat> myFleet;
+    private String turn;
+    private AI rival;
+    private ArrayList checkHits;
+    private int counter;
+    private View customToastRoot;
+    private TextView msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        // Initialize Toast view for player turn information
+        LinearLayout styledToast = findViewById(R.id.main_layout);
+        LayoutInflater inflater = getLayoutInflater();
+        customToastRoot = inflater.inflate(R.layout.toast, styledToast, false);
+        msg = (TextView) customToastRoot.findViewById(R.id.txtToast);
+
         boatMgr = new BoatMgr(this);
+        rival = new AI(0);
 
         /**
          * Create the boards and place boats
@@ -56,14 +68,13 @@ public class GameActivity extends AppCompatActivity {
         final int childCount = rivalLayout.getChildCount();
         for (int i = 0; i < childCount; i++) {
             final LinearLayout linearLayout = (LinearLayout) rivalLayout.getChildAt(i);
-            //final View imageview = linearLayout.getChildAt(0);
             linearLayout.setOnClickListener(myListener);
         }
 
         // Create rival fleet
         rivalFleet = createRivalFleet();
         for(int i = 0; i < rivalFleet.size(); i++) {
-            Log.i("boat", String.valueOf(rivalFleet.get(i)));
+            Log.i("BT boat", String.valueOf(rivalFleet.get(i)));
         }
 
         // Place rival fleet
@@ -75,60 +86,69 @@ public class GameActivity extends AppCompatActivity {
         myBoard.createBoard(this,myLayout);
 
         // Get player fleet
-        ArrayList<Boat> myFleet = boatMgr.getAll("player");
+        myFleet = boatMgr.getAll("player");
         for(int i = 0; i < myFleet.size(); i++) {
-            Log.i("boat", String.valueOf(myFleet.get(i)));
+            Log.i("BT boat", String.valueOf(myFleet.get(i)));
         }
 
         // Place player fleet
         placeFleet(myFleet, myLayout);
+
+        // Initialize turn, checks and counter
+        turn = "player";
+        msg.setText("A toi de commencer");
+        msg.setVisibility(View.VISIBLE);
+        Toast customToast = new Toast(getApplicationContext());
+        customToast.setView(customToastRoot);
+        customToast.setDuration(Toast.LENGTH_SHORT);
+        customToast.setGravity(Gravity.TOP, 0, 0);
+        customToast.show();
+
+        checkHits = new ArrayList();
+        counter = 0;
     }
 
     View.OnClickListener myListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            String tag = String.valueOf(v.getTag());
-            LinearLayout linearLayout = (LinearLayout) v;
-            ImageView imageView = (ImageView)linearLayout.getChildAt(0);
-            int idImage = R.drawable.missed;
+            if (turn.equals("player")) {
+                String tag = String.valueOf(v.getTag());
+                LinearLayout linearLayout = (LinearLayout) v;
+                ImageView imageView = (ImageView) linearLayout.getChildAt(0);
 
-            for(int i = 0; i < rivalFleet.size(); i++) {
-                for(int j = 0; j < rivalFleet.get(i).getPosition().size(); j++) {
+                fireHit(tag, imageView, rivalFleet, rivalLayout);
 
-                    if(tag.equals(String.valueOf(rivalFleet.get(i).getPosition().get(j)))) {
-                        //if view is boat set background image to explosion and modify boat state
-                        idImage = R.drawable.explosion;
-                        int life = rivalFleet.get(i).getLife();
-                        life--;
-                        if(life != 0) {
-                            rivalFleet.get(i).setLife(life);
-                        } else {
-                            final int childCount = rivalLayout.getChildCount();
-                            for(int k = 0; k < rivalFleet.get(i).getPosition().size(); k++) {
-                                for(int l = 0; l < childCount; l++) {
-                                    final LinearLayout ll = (LinearLayout) rivalLayout.getChildAt(l);
-                                    final String tagLl = String.valueOf(ll.getTag());
-                                    if(tagLl.equals(String.valueOf(rivalFleet.get(i).getPosition().get(k)))) {
-                                        ll.setBackgroundColor(Color.parseColor("#FF0000"));
-                                    }
-                                }
-                            }
-                            // TODO : check for endgame
-                        }
+                v.setOnClickListener(null);
+
+                turn = "rival";
+
+                msg.setText("L'ennemi attaque !");
+                Toast customToast = new Toast(getApplicationContext());
+                customToast.setView(customToastRoot);
+                customToast.setDuration(Toast.LENGTH_SHORT);
+                customToast.setGravity(Gravity.TOP, 0, 0);
+                customToast.show();
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        rivalAim(rival.getLevel());
+
                     }
-                }
+                }, 2000);
             }
-            imageView.setImageResource(idImage);
         }
     };
 
     public ArrayList<Boat> createRivalFleet() {
         ArrayList<Boat> rivalFleet = new ArrayList<Boat>();
-        rivalPorteAvion = new Boat("rival","porteAvion",6,6);
-        rivalCroiseur = new Boat("rival","croiseur",3,3);
-        rivalContreTorpilleur1 = new Boat("rival","1contreTorpilleur",5,5);
-        rivalContreTorpilleur2 = new Boat("rival","2contreTorpilleur",5,5);
-        rivalTorpilleur = new Boat("rival","torpilleur",4,4);
+        Boat rivalPorteAvion = new Boat("rival","porteAvion",6,6);
+        Boat rivalCroiseur = new Boat("rival","croiseur",3,3);
+        Boat rivalContreTorpilleur1 = new Boat("rival","1contreTorpilleur",5,5);
+        Boat rivalContreTorpilleur2 = new Boat("rival","2contreTorpilleur",5,5);
+        Boat rivalTorpilleur = new Boat("rival","torpilleur",4,4);
         rivalFleet.add(rivalPorteAvion);
         rivalFleet.add(rivalCroiseur);
         rivalFleet.add(rivalContreTorpilleur1);
@@ -139,8 +159,7 @@ public class GameActivity extends AppCompatActivity {
         return rivalFleet;
     }
 
-    public ArrayList<Boat> setRandomPlacement(ArrayList<Boat> fleet) {
-        // TODO : bug, boat over boat
+    public void setRandomPlacement(ArrayList<Boat> fleet) {
         // Get possible orientations
         ArrayList<String> orientations = new ArrayList<>();
         orientations.add("horizontal_left");
@@ -170,6 +189,7 @@ public class GameActivity extends AppCompatActivity {
                 int row = ThreadLocalRandom.current().nextInt(0, Board.ROWS);
                 int col = ThreadLocalRandom.current().nextInt(0, Board.COLUMNS);
                 int size = fleet.get(i).getSize();
+                //Log.i("BT boat", String.valueOf(fleet.get(i).getName()));
 
                 if (size % 2 == 0) {
                     midTile = (int) size / 2;
@@ -177,12 +197,12 @@ public class GameActivity extends AppCompatActivity {
                     midTile = (int) Math.ceil(size / 2);
                 }
 
-                // Get placement of each tile of boat according to orientation and midTile position
+                // Set placement of each tile of boat according to orientation and midTile position
                 for (int j = 0; j < size; j++) {
                     int newRow = 0;
                     int newCol = 0;
                     int diffOrder = midTile - j;
-
+                    //Log.i("BT tile", String.valueOf("tile"+j));
                     if (diffOrder > 0) {
                         if (orientation == "horizontal_left") {
                             newRow = row;
@@ -218,13 +238,19 @@ public class GameActivity extends AppCompatActivity {
 
                     // Check validity of tile position
                     tileTag = "row" + newRow + "col" + newCol;
-                    if(newRow < 0 || newRow > 9 || newCol < 0 || newRow > 9) {
+                    //Log.i("BT checkedTag", tileTag);
+                    if(newRow < 0 || newRow > 9 || newCol < 0 || newCol > 9) {
                         flag = false;
+                        //Log.i("BT flag", String.valueOf(flag));
+                        break;
                     } else if(checkValidityOfNewTilePosition(tileTag, fleet)) {
                         positions.add(tileTag);
                         flag = true;
+                        //Log.i("BT flag", String.valueOf(flag));
                     } else {
                         flag = false;
+                        //Log.i("BT flag", String.valueOf(flag));
+                        break;
                     }
                 }
             } while (!flag);
@@ -234,7 +260,6 @@ public class GameActivity extends AppCompatActivity {
             fleet.get(i).setPosition(positions);
 
         }
-        return fleet;
     }
 
     private void placeFleet(ArrayList<Boat> fleet, GridLayout layout) {
@@ -245,6 +270,7 @@ public class GameActivity extends AppCompatActivity {
                     final LinearLayout ll = (LinearLayout) layout.getChildAt(k);
                     final String tagLl = String.valueOf(ll.getTag());
                     if(tagLl.equals(String.valueOf(fleet.get(i).getPosition().get(j)))) {
+                        // TODO : set boat bitmaps on positions and orientation
                         ll.setBackgroundColor(Color.parseColor("#FFFFFF"));
                     }
                 }
@@ -253,20 +279,140 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private boolean checkValidityOfNewTilePosition(String tag, ArrayList<Boat> fleet) {
-        //Log.i("checkNewTilePosition", "true");
-        //Log.i("Fleet size", String.valueOf(fleet.size()));
         for(int i = 0; i < fleet.size(); i++) {
-            //Log.i("Boat", String.valueOf(fleet.get(i)));
             ArrayList positions = fleet.get(i).getPosition();
             for(int j = 0; j < positions.size(); j++) {
-                //Log.i("newTag", tag);
-                //Log.i("existing tag", String.valueOf(positions.get(j)));
-                if(tag == String.valueOf(positions.get(j))) {
+                if(tag.equals(String.valueOf(positions.get(j)))) {
+                    Log.i("BT flag", "false");
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    private void rivalAim(int level) {
+        if(turn.equals("rival")) {
+            String tileTag;
+
+            if (level == 0) {
+                do {
+                    int row = ThreadLocalRandom.current().nextInt(0, Board.ROWS);
+                    int col = ThreadLocalRandom.current().nextInt(0, Board.COLUMNS);
+                    tileTag = "row" + row + "col" + col;
+                } while (checkHits.contains(tileTag));
+
+                checkHits.add(tileTag);
+
+                LinearLayout linearLayout = (LinearLayout) getViewByTag(tileTag);
+                ImageView imageView = (ImageView) linearLayout.getChildAt(0);
+
+                fireHit(tileTag, imageView, myFleet, myLayout);
+
+            }
+        }
+        turn = "player";
+        msg.setText("A ton tour !");
+        Toast customToast = new Toast(getApplicationContext());
+        customToast.setView(customToastRoot);
+        customToast.setDuration(Toast.LENGTH_SHORT);
+        customToast.setGravity(Gravity.TOP, 0, 0);
+        customToast.show();
+    }
+
+    private View getViewByTag(String tag) {
+        LinearLayout view = null;
+        final int childCount = myLayout.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            view = (LinearLayout) myLayout.getChildAt(i);
+            final String tagObj = String.valueOf(view.getTag());
+            if (tagObj.equals(tag)) {
+                break;
+            }
+        }
+        return view;
+    }
+
+    private void fireHit(String tag, ImageView imageView, ArrayList<Boat> fleet, GridLayout layout) {
+        int idImage = R.drawable.missed;
+
+        for (int i = 0; i < fleet.size(); i++) {
+            for (int j = 0; j < fleet.get(i).getPosition().size(); j++) {
+
+                if (tag.equals(String.valueOf(fleet.get(i).getPosition().get(j)))) {
+                    //if view is boat set background image to explosion and modify boat state and life
+                    idImage = R.drawable.explosion;
+                    int life = fleet.get(i).getLife();
+                    life--;
+                    //Log.i("BT life", String.valueOf(life));
+                    if (life != 0) {
+                        fleet.get(i).setLife(life);
+                        fleet.get(i).setState(1);
+                    } else {
+                        fleet.get(i).setState(2);
+                        final int childCount = layout.getChildCount();
+                        for (int k = 0; k < fleet.get(i).getPosition().size(); k++) {
+                            for (int l = 0; l < childCount; l++) {
+                                final LinearLayout ll = (LinearLayout) layout.getChildAt(l);
+                                final String tagLl = String.valueOf(ll.getTag());
+                                if (tagLl.equals(String.valueOf(fleet.get(i).getPosition().get(k)))) {
+                                    ll.setBackgroundColor(Color.parseColor("#FF0000"));
+                                }
+                            }
+                        }
+                        checkForEndGame(fleet);
+                    }
+                }
+            }
+        }
+        imageView.setImageResource(idImage);
+    }
+
+    private void checkForEndGame(ArrayList<Boat> fleet) {
+        boolean flag = true;
+        String team = fleet.get(0).getTeam();
+
+        for(int i = 0; i < fleet.size(); i++) {
+            int state = fleet.get(i).getState();
+            if(state != 2) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            endGame(team);
+        }
+    }
+
+    private void endGame(String losingTeam) {
+        AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        if(losingTeam.equals("rival")) {
+            // alerte Vous avez gagné
+            builder.setMessage("Vous avez gagné !");
+        } else {
+            // alerte vous avez perdu
+            builder.setMessage("Vous avez perdu...");
+        }
+        // alerte Recommencer ? Quitter ?
+        builder.setCancelable(false);
+
+        builder.setNegativeButton("Recommencer", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(GameActivity.this, PlacementActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        builder.setPositiveButton("Quitter", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                GameActivity.this.finish();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     /**
